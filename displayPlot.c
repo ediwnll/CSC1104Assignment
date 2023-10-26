cce7241e858cdcfb43a3035db7f6a1a18cf45163
Edwin <hewzhongxuan6f@gmail.com>
2023-10-26 16:46:21 +0800

    Auto stash before merge of "main" and "origin/main"

--- displayPlot.c
+++ displayPlot.c
@@ -19,43 +19,46 @@ and displays the data in graphs using GNUplot
 void plot_gnu_step();
 void testplot();
 
-void testplot(){
+void testplot()
+{
     // Gnuplot commands
-    const char* gnuplotCommands[] = {
-        "set multiplot layout 2, 1",    // Create a 2 plot in 1 window gnuplot
-        "set tmargin at screen 0.85",   //Adjust top margin to space the plot out
-        "set bmargin at screen 0.45",   //adjust bottom margin to space the plot
-        "set xrange [0:300]",           //set the x-axis of range, to be used with dataset
-        "set tics scale 0.75",          //set tics scaling for labels
-        "set xtics 20",                 //set x-axis to be at intervals of 20
-        "set grid xtics",               //display grid lines for x axis tics
-        "set format x ''",              //remove x axis label format
-        "set ytics 0,1",                //set y-axis tics at 0 and 1
-        "set grid ytics",               //display gridline for y axis
-        "set format y '%.1f'",          //format y axis label
-        "s1(x) = (int(x) % 20 < 10) ? 1 : 0",   //define step1 as a step function
-        "s2(x) = (int(x) % 40 < 20) ? 1 : 0",   //define step2 as a step function
-        "set yrange [-0.5:1.5]",        //set y axis range to be -0.5 to 1.5
-        "set samples 1000",             //set sample for smooth curves
-        "plot s1(x) with steps ls 1 title 'Graph 1'",   //plot s1 with the title
-        "set bmargin at screen 0.1",    //adjust bottom margin of bottom graph
-        "set tmargin at screen 0.45",   //adjust top margin of bottom graph
-        "set yrange [-0.5:1.5]",        //set y axis range to be -0.5 to 1.5
-        "set format x '%.0f'",          //set x axis label format
-        "plot s2(x) with steps ls 1 title 'Graph 2'",   //plot s2 with the title
-        "unset multiplot",  // Exit multiplot mode
+    const char *gnuplotCommands[] = {
+        "set multiplot layout 2, 1",                  // Create a 2 plot in 1 window gnuplot
+        "set tmargin at screen 0.85",                 // Adjust top margin to space the plot out
+        "set bmargin at screen 0.45",                 // adjust bottom margin to space the plot
+        "set xrange [0:300]",                         // set the x-axis of range, to be used with dataset
+        "set tics scale 0.75",                        // set tics scaling for labels
+        "set xtics 20",                               // set x-axis to be at intervals of 20
+        "set grid xtics",                             // display grid lines for x axis tics
+        "set format x ''",                            // remove x axis label format
+        "set ytics 0,1",                              // set y-axis tics at 0 and 1
+        "set grid ytics",                             // display gridline for y axis
+        "set format y '%.1f'",                        // format y axis label
+        "s1(x) = (int(x) % 20 < 10) ? 1 : 0",         // define step1 as a step function
+        "s2(x) = (int(x) % 40 < 20) ? 1 : 0",         // define step2 as a step function
+        "set yrange [-0.5:1.5]",                      // set y axis range to be -0.5 to 1.5
+        "set samples 1000",                           // set sample for smooth curves
+        "plot s1(x) with steps ls 1 title 'Graph 1'", // plot s1 with the title
+        "set bmargin at screen 0.1",                  // adjust bottom margin of bottom graph
+        "set tmargin at screen 0.45",                 // adjust top margin of bottom graph
+        "set yrange [-0.5:1.5]",                      // set y axis range to be -0.5 to 1.5
+        "set format x '%.0f'",                        // set x axis label format
+        "plot s2(x) with steps ls 1 title 'Graph 2'", // plot s2 with the title
+        "unset multiplot",                            // Exit multiplot mode
     };
 
     // Open a pipe to Gnuplot
-    FILE* gnuplotPipe = popen("gnuplot -persistent", "w");
-    //if no gnuplot, exit
-    if (gnuplotPipe == NULL) {
+    FILE *gnuplotPipe = popen("gnuplot -persistent", "w");
+    // if no gnuplot, exit
+    if (gnuplotPipe == NULL)
+    {
         perror("Could not open Gnuplot.");
         exit(EXIT_FAILURE);
     }
 
     // Execute Gnuplot commands
-    for (int i = 0; i < sizeof(gnuplotCommands) / sizeof(gnuplotCommands[0]); i++) {
+    for (int i = 0; i < sizeof(gnuplotCommands) / sizeof(gnuplotCommands[0]); i++)
+    {
         fprintf(gnuplotPipe, "%s\n", gnuplotCommands[i]);
     }
 
@@ -72,7 +75,7 @@ void plot_gnu_step()
         exit(EXIT_FAILURE);
     }
     double x[] = {0.0, 20.0, 40.0, 60.0, 80.0, 100.0, 120.0, 140.0, 160.0, 180.0, 200.0, 220.0, 240.0, 260.0, 280.0, 300.0};
-    int dataA[] = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+    int dataA[] = {1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1};
     int dataB[] = {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
 
     // Send Gnuplot commands to plot both data sets
@@ -82,7 +85,7 @@ void plot_gnu_step()
     fprintf(gnuplotPipe, "plot '-' with steps title 'Data A', '-' with steps title 'Data B'\n");
     for (int i = 0; i < 16; i++)
     {
-        fprintf(gnuplotPipe, "%lf %d\n", x[i], dataA[i]);
+        fprintf(gnuplotPipe, "%lf %d\n", x[i], dataA[0]);
     }
     fprintf(gnuplotPipe, "e\n");
     for (int i = 0; i < 16; i++)
